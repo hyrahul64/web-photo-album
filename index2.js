818db23aef11bdb4def751ee6275c5a15e3366fc
Rahul Shah <hyrahul64@gmail.com>
2023-11-01 22:52:09 +0530

    Refactored downloading into separate functions issues - some images' download fail & unordered download

--- index2.js
+++ index2.js
@@ -1,16 +1,17 @@
 function onLoad() {
-    console.log("log from onLoad() function")
+    //console.log("log from onLoad() function")
     displayImages();    
 }
+
 async function displayImages() {
-    console.log("log from displayImages() function")
+    //console.log("log from displayImages() function")
     const access_token = await getAccessToken();
-    console.log(access_token);
+    //console.log(access_token);
     fetchImages(access_token);
 }
 
 async function getAccessToken() {
-    console.log("log from getAccessToken() function")
+    //console.log("log from getAccessToken() function")
     const client_id = "55220f20-0882-4457-9e11-9f12d7d663ed";
     const scope = "onedrive.readwrite offline_access";
     const redirect_uri = "http://localhost:5500";
@@ -18,7 +19,7 @@ async function getAccessToken() {
         `client_id=${client_id}&scope=${scope}&response_type=token&redirect_uri=${redirect_uri}`;
     let resp = await fetch(one_drive_auth_endpoint);
     if(resp.status == 200) {
-        console.log("response received.")
+        //console.log("response received.")
         let inurl_resp = window.location.hash;
         //console.log(inurl_resp);
         let access_token = inurl_resp.split("&")[0].split("=")[1];
@@ -29,9 +30,8 @@ async function getAccessToken() {
     }
 }
 
-
 async function fetchImages(access_token) {
-    console.log("log from fetchImages() function");
+    //console.log("log from fetchImages() function");
     const image_source_endpoint = "https://api.onedrive.com/v1.0/drives/15d9078b36f5bc5a/items" 
         + "/15D9078B36F5BC5A!6692/children?select=name,@content.downloadUrl";
     const resp = await fetch(image_source_endpoint,
@@ -45,32 +45,40 @@ async function fetchImages(access_token) {
     //console.log(resp);
     const json_resp = await resp.json();
     const imageparams_list = json_resp.value;
-    console.log(imageparams_list);
+    console.log("Total images:" + imageparams_list.length);
+    //console.log(imageparams_list);
     for(var i=0; i < imageparams_list.length; i++) {
-        let image_download_endpoint = Object.values(imageparams_list[i])[0];
-        let image_download_name = Object.values(imageparams_list[i])[1];
-        let start_time = Date.now();
-        let resp  = await fetch(image_download_endpoint,
-            {
-                method: "GET",
-                headers:{
-                    Authorization:access_token
-                }
-            });
-        let end_time = Date.now();
-        console.log("time interval:" + (end_time - start_time));
-        if(resp.status = 200) {
-            console.log("image downloded:" + image_download_name);
-            const blob = await resp.blob();
-            const objectUrl = URL.createObjectURL(blob);
-            const imgDiv = document.createElement('div')
-            imgDiv.className = 'grid-image';
-            imgDiv.innerHTML=`<p>Image-${i}</p><img src="${objectUrl}"/>`
-            document.getElementById('content-grid').appendChild(imgDiv);
-        } else {
-            console.log("Error in loading image:" + image_download_name);
-        }
-
+        fetchImage(imageparams_list[i]);
     }
 
+}
+
+async function fetchImage(imageparam, access_token) {
+    let image_download_endpoint = Object.values(imageparam)[0];
+    let image_download_name = Object.values(imageparam)[1];
+    let start_time = Date.now();
+    let resp  = await fetch(image_download_endpoint,
+        {
+            method: "GET",
+            headers:{
+                Authorization:access_token
+            }
+        });
+    let end_time = Date.now();
+    //console.log("time interval:" + (end_time - start_time));
+    if(resp.status = 200) {
+        //console.log("image downloded:" + image_download_name);
+        const blob = await resp.blob();
+        displayImage(blob);
+    } else {
+        console.log("Error in loading image:" + image_download_name);
+    } 
+}
+
+async function displayImage(blob) {
+    const objectUrl = URL.createObjectURL(blob);
+    const imgDiv = document.createElement('div')
+    imgDiv.className = 'grid-image';
+    imgDiv.innerHTML=`<p>Image</p><img src="${objectUrl}"/>`
+    document.getElementById('content-grid').appendChild(imgDiv); 
 }
